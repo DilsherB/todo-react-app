0640644db15033436ea8f7d02b1e3042700923ae
Dilsher Balouch <dilsherb@gmail.com>
2023-04-03 17:03:28 +0300

    TodosList component added.

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,13 +1,13 @@
-import TodoItem from "./TodoItem";
-
-const TodosList = ({ todosProps }) => {
+import TodoItem from "@/components/TodoItem";
+import { useTodosContext } from "@/context/TodosContext";
+const TodosList = () => {
+  const { todos } = useTodosContext();
   return (
     <ul>
-      {todosProps.map((todo) => (
+      {todos.map((todo) => (
         <TodoItem key={todo.id} itemProp={todo} />
       ))}
     </ul>
-  )
-}
-
+  );
+};
 export default TodosList;
